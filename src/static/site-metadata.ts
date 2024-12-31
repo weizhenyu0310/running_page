27422b13ef7bc10c94d6b5065611c506023361c0
weizhenyu0310 <55968391+weizhenyu0310@users.noreply.github.com>
2024-12-31 16:08:21 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,8 +11,8 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Weizhenyu Running',
-  siteUrl: 'https://weizhenyu0310.github.io/running/',
-  logo: 'http://img-20210107.oss-cn-beijing.aliyuncs.com/img/微信截图_20241231155119.png',
+  siteUrl: 'https://weizhenyu0310.github.io/running_page/',
+  logo: 'https://img-20210107.oss-cn-beijing.aliyuncs.com/img/微信截图_20241231155119.png',
   description: 'Personal site and blog',
   navLinks: [
     {
